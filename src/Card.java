7669fd05a9d3eb9b9279ea9b0fcab0ccbb363fd6
Mark Lawson <mark@walkonmars.co.uk>
2022-07-05 12:11:12 +0100

    third commit

--- src/Card.java
+++ src/Card.java
@@ -1,10 +1,31 @@
 public class Card {
 
 //    HAS A STRING SUIT.. USE THE UNICODE CHARACTERS OF HEART, CLUB, DIAMOND AND SPADE
+    private String cardName, suit;
 
+    public Card(String cardName, String suit) {
+        this.cardName = cardName;
+        this.suit = suit;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
 //    HAS A STRING SYMBOL (2,3,4,5,6,7,8,9,10,J,Q,K,A)
 
-//    HAS AN INT VALUE (2,3,4,5,6,7,8,9,10,11)
+//    HAS AN INT VALUE (2,3,4,5,6,7,8,9,10,11,12,13,14)
 
 //    HAS A toString METHOD THAT DESCRIBES THE CLASS
 
--- src/CardGame.java
+++ src/CardGame.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class CardGame {
 
 //    USING CLASSES, ARRAY LISTS AND METHODS, CREATE A DECK OF CARDS. YOU SHOULD USE THE FOLLOWING CLASSES
 //    CARD GAME
-
+//    CARD
 
 
 
@@ -11,5 +13,28 @@ public class CardGame {
 
 //    HAS A NAME WHICH IS ALSO DEFINED IN THE CONSTRUCTOR
 
+
 //    HAS A GET DECK METHOD THAT LISTS OUT THE CARDS IN THE DECK
+
+
+
+    //    CONTAINS AN ARRAYLIST<CARD> FOR THE DECK OF CARDS THAT CONTAINS ALL 52 CARDS.
+
+    ArrayList<Integer> cardDeck = new ArrayList<Integer>();
+
+    public void createDeck() {
+       for (int i = 0; i < 4; i++) {
+           for (int j = 0; j < 14; j++) {
+               cardDeck.add(j);
+           }
+       }
+
+   }
+
+
+
+
+
+
+
 }
